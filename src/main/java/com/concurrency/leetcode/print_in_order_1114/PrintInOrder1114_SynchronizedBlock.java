ac3f016899a61f47fed1c0ce041529f3c6356d99
Zhek <Zhekfake@gmail.com>
2023-04-21 20:10:31 +0300

    added PrintInOrder1114_SemaphoreTest

---
+++ src/main/java/com/concurrency/leetcode/print_in_order_1114/PrintInOrder1114_SynchronizedBlock.java
@@ -0,0 +1,57 @@
+package com.concurrency.leetcode.print_in_order_1114;
+
+public class PrintInOrder1114_SynchronizedBlock {
+
+    public static class Foo {
+
+        private String mutex = "";
+        private int counter = 1;
+
+        public String result = "";
+
+        public Foo() {
+        }
+
+        public void first(Runnable printFirst) {
+            boolean loop = true;
+            while (loop) {
+                synchronized (mutex) {
+                    if (counter == 1) {
+                        // printFirst.run() outputs "first". Do not change or remove this line.
+                        printFirst.run();
+                        ++counter;
+                        loop = false;
+                    }
+                }
+            }
+        }
+
+        public void second(Runnable printSecond) {
+            boolean loop = true;
+            while (loop) {
+                synchronized (mutex) {
+                    if (counter == 2) {
+                        // printSecond.run() outputs "second". Do not change or remove this line.
+                        printSecond.run();
+                        ++counter;
+                        loop = false;
+                    }
+                }
+            }
+        }
+
+        public void third(Runnable printThird) {
+            boolean loop = true;
+            while (loop) {
+                synchronized (mutex) {
+                    if (counter == 3) {
+                        // printThird.run() outputs "third". Do not change or remove this line.
+                        printThird.run();
+                        ++counter;
+                        loop = false;
+                    }
+                }
+            }
+        }
+    }
+}
